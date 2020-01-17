513f4e3b5383767877be9a41d35f7e7dd3ee5e0d
CARRERA PROFESIONAL <CARRERA PROFESIONAL@PC06-LAB1>
2020-01-17 15:46:42 -0500

    vercion 3.0

---
+++ SOSAQP/SOSAQP/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOSAQP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Tu pagina de inicio";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SOSAQP/SOSAQP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SOSAQP.MvcApplication" Language="C#" %>
